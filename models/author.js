a1f123caf00f0a0e1064ec6099801742d76ca6fd
kunshao-msft <kunshao@umich.edu>
2018-04-19 15:29:41 -0700

    render author and genre

--- models/author.js
+++ models/author.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 
+var moment = require('moment');
+
 var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema(
@@ -24,6 +26,20 @@ AuthorSchema
 .get(function () {
   return '/catalog/author/' + this._id;
 });
+
+
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+  return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+});
+
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+  return this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
+});
+
 
 //Export model
 module.exports = mongoose.model('Author', AuthorSchema);
